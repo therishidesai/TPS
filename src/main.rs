349912f726052e99771eea4c4815f09eb873724c
Rishi Desai <desai.rishi1@gmail.com>
2020-06-27 18:20:32 -0700

    moved threadpool to a separate library and it works correctly

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 extern crate tps;
 
-use std::thread;
-use std::sync::{Arc, Mutex};
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::collections::VecDeque;
+use tps::threadpool::ThreadPool;
 
 /*
 TPS implemtnation idea
@@ -21,61 +18,20 @@ TPS implemtnation idea
      and executes the callback with the published data
  */
 
-// Callback experiments
-
-fn apply<F>(f: F) where
-	F: Fn() {
-	f();
-}
+// ThreadPool Experiment
 
 fn simple_callback1(x: i32) {
     println!("hello world, {} is a number", x);
 }
 
 fn main() {
-	let buf = Arc::new(Mutex::new(VecDeque::new()));
-	let stop_flag = Arc::new(AtomicBool::new(false));
+	let mut tp = ThreadPool::new(3);
 	
-	let mut children = vec![];
-	let workers = 3;
-
-	for worker in 0..workers{
-		// ref count for the workqueue for this worker thread
-		let stop_flagn = Arc::clone(&stop_flag);
-		let bufn = buf.clone();
-		children.push(thread::spawn(move ||{
-			loop {
-				let mut workqueue = bufn.lock().unwrap();
-				let work_maybe = workqueue.pop_front();
-				match work_maybe {
-					None => {
-						if stop_flagn.load(Ordering::Relaxed) {
-							break;
-						} else {
-							continue;
-						}
-					},
-					Some(work) => {
-						print!("Thread{}: ", worker);
-						apply(work);
-					},
-				}
-			}
-		}));
-		
-	}
-
 	for x in 0..100 {
 		// Capture `x` into an anonymous type and implement
 		// `Fn` for it. Store it in `print`.
-		let mut workqueue = buf.lock().unwrap();
-		workqueue.push_back(move || simple_callback1(x));
+		tp.push_work(move || simple_callback1(x));
 	}
 
-	stop_flag.swap(true, Ordering::Relaxed);
-
-	for child in children {
-        // Wait for the thread to finish. Returns a result.
-        let _ = child.join();
-    }
+	tp.stop();
 }
---
+++ src/threadpool.rs
@@ -0,0 +1,73 @@
+use std::thread;
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::collections::VecDeque;
+
+
+pub struct ThreadPool {
+	workers: Vec<Option<std::thread::JoinHandle<()>>>,
+	stop_flag: Arc<AtomicBool>,
+	workqueue: Arc<Mutex<VecDeque<Box<Fn() + Send + 'static>>>>
+}
+
+fn apply<F>(f: F) where
+	F: Fn() {
+	f();
+}
+
+impl ThreadPool {
+	pub fn new(size: usize) -> ThreadPool {
+		assert!(size > 0);
+		let mut workers = Vec::with_capacity(size);
+		let stop_flag = Arc::new(AtomicBool::new(false));
+		let workqueue = Arc::new(Mutex::new(VecDeque::new()));
+
+		for worker in 0..size {
+			// ref count for the workqueue for this worker thread
+			let stop_flagn = Arc::clone(&stop_flag);
+			let bufn = workqueue.clone();
+			workers.push(Some(thread::spawn(move ||{
+				loop {
+					let mut wq = bufn.lock().unwrap();
+					let work_maybe = wq.pop_front();
+					match work_maybe {
+						None => {
+							if stop_flagn.load(Ordering::Relaxed) {
+								break;
+							} else {
+								continue;
+							}
+						},
+						Some(work) => {
+							print!("Thread{}: ", worker);
+							apply(work);
+						},
+					}
+				}
+			})));
+		}
+
+		ThreadPool {
+			workers,
+			stop_flag,
+			workqueue,
+		}
+	}
+
+	pub fn stop(&mut self) {
+		self.stop_flag.swap(true, Ordering::Relaxed);
+
+		for worker in &mut self.workers {
+			// Wait for the thread to finish. Returns a result.
+			if let Some(thread) = worker.take() {
+				thread.join().unwrap();
+			}
+		}
+	}
+
+	pub fn push_work<F>(&self, f: F) where
+		F: Fn() + Send + 'static{
+		let mut wq = self.workqueue.lock().unwrap();
+		wq.push_back(Box::new(f));
+	}
+}
